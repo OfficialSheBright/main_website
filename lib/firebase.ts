cef7d51ec1ada9f41befda684b5b231e5617973a
shebright <officialshebright@gmail.com>
2025-10-29 23:04:59 +0530

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  NEXT_PUBLIC_FIREBASE_API_KEY=AIzaSyDF_kxqcS_V6cCCHGT0G-WAZCv8uDwKc34
+  NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN=shebright-66fe1.firebaseapp.com
+  NEXT_PUBLIC_FIREBASE_PROJECT_ID=shebright-66fe1
+  NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET=shebright-66fe1.firebasestorage.app
+  NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID=708792241069 
+  NEXT_PUBLIC_FIREBASE_APP_ID=1:708792241069:web:cd1ab0a6eff724af8a2f24
 };
 
 // Check if Firebase is already initialized to avoid multiple initializations
