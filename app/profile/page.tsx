42003d2bfca506525b8c59237ff53fef8080d2ec
Ghanshyam Singh <ghanshyam2005singh@gmail.com>
2025-11-23 10:19:45 +0530

    lint fix

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -90,7 +90,7 @@ export default function Profile() {
     certificatesEarned: 0
   });
   const [loadingData, setLoadingData] = useState(false);
-  const router = useRouter();
+ // const router = useRouter();
 
   useEffect(() => {
     if (user) {
